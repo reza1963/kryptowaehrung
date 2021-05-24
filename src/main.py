5248d6236f364a2aeed0e10f0cecb5109a89baa9
Reza <ri@multilix.com>
2021-05-24 20:27:54 +0200

    first commit

--- src/main.py
+++ src/main.py
@@ -3,11 +3,21 @@ from src.util.pandaz import Pandaz
 pandaz = Pandaz("data/")
 m, p, s = pandaz.dataframes()
 
-pandaz.box_plot(s, "supply")
-pandaz.box_plot(m, "mcap")
-pandaz.box_plot(p, "price")
+print(" ***** mcap ***")
+print(m)
+
+print(" ***** price ***")
+print(p)
+
+print(" ***** supply ***")
+print(s)
+
+#pandaz.box_plot(s, "supply")
+#pandaz.box_plot(m, "mcap")
+#pandaz.box_plot(p, "price")
 
 p = pandaz.group_by_cur(p)
-
 merged_df = pandaz.merge(p, m)
+
+print(" ***** merged ***")
 print(merged_df)
